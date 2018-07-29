cdb1c3de6e26aa61a81669baf9f77c87bbfedd9d
Saulo Reis <sauloarth@gmail.com>
2018-07-29 12:34:51 -0300

    Limitando as chamadas com takeUntil.

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,10 @@
 import { log } from "./utils/promise-helpers.js";
 import './utils/array-helpers.js';
 import { notasService as service } from './notas/service.js'
+import { takeUntil } from './utils/operators.js'
+
+const operation = takeUntil(3, () => service.sumItems('2143').then(console.log).catch(console.log));
 
 document
     .querySelector('#myButton')
-    .onclick = () =>
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log);
+    .onclick = operation;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -5,3 +5,12 @@ export const compose = (...fns) => param =>
 
 export const pipe = (...fns) => param =>
     fns.reduce((previousReturn, currentFunction) => currentFunction(previousReturn), param);
+
+// takeUntil é uma variável que recebe uma função
+// a função recebe dois parâmetros e retorna outra função
+// a função retornada não recebe nenhum parâmetro
+// a função retornada lembra-se de time e fn (closure)
+// a função retornada executa fn conforme o valor de times.
+
+export const takeUntil = (times, fn) =>
+    () => times-- > 0 && fn();
