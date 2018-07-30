5751e8cbf593190bd99b897d4965427dbd206da2
Saulo Reis <sauloarth@gmail.com>
2018-07-30 11:47:59 -0300

    Utilizando promise race para prevenir lentidões na rede.

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-import { log } from "./utils/promise-helpers.js";
+import { log, timeoutPromisse } from "./utils/promise-helpers.js";
 import './utils/array-helpers.js';
 import { notasService as service } from './notas/service.js'
 import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js'
@@ -9,8 +9,7 @@ const operations = pipe(
 )
 
 const action = operations(() =>
-    service
-    .sumItems('2143')
+    timeoutPromisse(200, service.sumItems('2143'))
     .then(console.log)
     .catch(console.log)
 );
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -5,3 +5,15 @@ export const log = param => {
     console.log(param);
     return param;
 }
+
+export const timeoutPromisse = (interval, promise) => {
+
+    const timer = new Promise((resolve, reject) => {
+        setTimeout(() =>
+            reject(`Limite de tempo excedido: ${interval}`), interval);
+    })
+
+    return Promise.race([
+        timer, promise
+    ])
+}
