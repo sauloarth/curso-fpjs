24275649422da3e00dd818a3ab95d3f7f18fd6e4
Saulo Reis <sauloarth@gmail.com>
2018-07-31 15:51:09 -0300

    Protegendo o retorno de null ou undefined com monada.

--- public/app/notas/service.js
+++ public/app/notas/service.js
@@ -1,12 +1,13 @@
 import { handleStatus } from "../utils/promise-helpers.js";
 import { partialize, pipe } from "../utils/operators.js";
+import { Maybe } from "../utils/maybe.js";
 
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(notas => notas.itens); //devolve uma função que recebe notas e faz um $flatMap
-const filterItemsByCode = (code, itens) => itens.filter(item => item.codigo == code);
-const sumItemsValue = itens => itens.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = notasM => notasM.map(notas => notas.$flatMap(notas => notas.itens)); //devolve uma função que recebe notas e faz um $flatMap
+const filterItemsByCode = (code, itensM) => itensM.map(itens => itens.filter(item => item.codigo == code));
+const sumItemsValue = itensM => itensM.map(itens => itens.reduce((total, item) => total + item.valor, 0));
 
 
 export const notasService = {
@@ -14,6 +15,7 @@ export const notasService = {
     listAll() {
         return fetch(API)
             .then(handleStatus)
+            .then(notas => Maybe.of(notas))
             .catch(err => {
                 console.log(err);
                 return Promise.reject("Não foi possivel carregar notas.");
@@ -25,7 +27,9 @@ export const notasService = {
         const filterItems = partialize(filterItemsByCode, code);
         const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
 
-        return this.listAll().then(sumItems);
+        return this.listAll()
+            .then(sumItems)
+            .then(result => result.getOrElse(0));
     }
 
 }
